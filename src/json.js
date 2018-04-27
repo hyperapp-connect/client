88e9d3c9b69530e5eeb5ee59dfb75f43d196cd04
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-28 00:02:23 +0200

    add json

---
+++ src/json.js
@@ -0,0 +1,32 @@
+const isString = o => typeof o === "string"
+
+export const parse = msg => {
+  if (!isString(msg)) {
+    return msg
+  }
+
+  try {
+    return JSON.parse(msg)
+  } catch (e) {
+    return msg
+  }
+}
+
+export const stringify = msg => {
+  try {
+    if (isString(msg)) {
+      msg = JSON.parse(msg)
+    }
+
+    msg[0] = `${apiVersion}.${msg[0]}`
+
+    return JSON.stringify(msg)
+  } catch (e) {
+    error(e)
+  }
+}
+
+export default {
+  parse,
+  stringify,
+}

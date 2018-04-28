962f9acf376095af24d43eac5e95d5f289cc3f32
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-29 01:22:18 +0200

    add actions and state, change exports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import map from './mapActions'
-import socket from './ws'
+export { mapActions } from './mapActions'
+export * as connect from './ws'
+export { actions } from './actions'
+export { state } from './state'
 
 // default action for connected actions.
 // will expect the response from the server to be a slice of the state.
 // this slice will then be updated.
 export const rem = res => () => res
-
-export const connect = socket
-export const mapActions = map

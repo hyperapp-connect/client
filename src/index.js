53713674e0efff09084c6f8d7a422f0dd075bd16
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-29 03:42:33 +0200

    update exports

--- src/index.js
+++ src/index.js
@@ -2,9 +2,3 @@ export { mapActions } from './mapActions'
 export { connect, send } from './ws'
 export { actions } from './actions'
 export { state } from './state'
-
-const exp = connect
-exp.connect = connect
-exp.send = send
-
-export const ws = exp

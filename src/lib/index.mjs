c621665f5103586a9244d19b4363ad3fadd31021
Jascha Ehrenreich <jascha@jaeh.at>
2018-05-27 01:04:51 +0200

    add exports for lib dir

---
+++ src/lib/index.mjs
@@ -0,0 +1,6 @@
+export { error } from './error'
+export { json } from './json'
+export { mapActions } from './mapActions'
+export { router } from './router'
+export { store } from './store'
+export { connect } from './ws'

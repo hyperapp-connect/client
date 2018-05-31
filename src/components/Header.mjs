a922d3980d3af672d22ffbd8b2c29210feaaf759
Jascha Ehrenreich <jascha@jaeh.at>
2018-05-31 03:52:18 +0200

    add Header and update Link

--- src/components/Header.mjs
+++ src/components/Header.mjs
@@ -3,8 +3,13 @@ import { h } from '../app'
 import { Menu } from './Menu'
 import { Routes } from './Routes'
 
-export const Header = ({ menu, pages }) => (
+const Logo = ({ logo }) => (
+  <img src={ logo.src } alt={logo.alt || logo.title} href={logo.href} />
+)
+
+export const Header = ({ menu, pages, logo }) => (
   <header class="main">
+    { logo && <Logo logo={logo} /> }
     <Menu name={menu} />
 
     <Routes pages={pages} />
--- src/components/Link.mjs
+++ src/components/Link.mjs
@@ -1,7 +1,9 @@
 import { h } from '../app'
 
-export const Link = ({ to }, children) => (_, actions) => (
-  <a onclick={e => actions.router.go({ e, to })} href={to}>
+const getClass = ({ path, to }) => path === to ? 'active' : ''
+
+export const Link = ({ to }, children) => (state, actions) => (
+  <a onclick={e => actions.router.go({ e, to })} href={to} class={getClass(state.router.pathname, to)}>
     {children || to}
   </a>
 )
